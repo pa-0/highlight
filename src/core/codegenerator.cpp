449cebc1a6269ccc1a5f22cd08a5172294742d11
saalen <as@andre-simon.de>
2023-03-14 15:17:30 +0100

    updated changelog

--- src/core/codegenerator.cpp
+++ src/core/codegenerator.cpp
@@ -1279,17 +1279,13 @@ void CodeGenerator::initASStream() {
         streamIterator =  new astyle::ASStreamIterator ( in );
         formatter->init ( streamIterator );
 
-        if (currentSyntax->getDescription()=="C#")
-	    {
+        if (currentSyntax->getDescription()=="C#") {
 		    formatter->setSharpStyle();
-	    } else if (currentSyntax->getDescription()=="Java")
-	    {
+	    } else if (currentSyntax->getDescription()=="Java") {
 		    formatter->setJavaStyle();
-	    } else if (currentSyntax->getDescription()=="Javascript")
-	    {
+	    } else if (currentSyntax->getDescription()=="Javascript") {
 		    formatter->setJSStyle();
-	    } else if (currentSyntax->getDescription()=="Objective C")
-        {
+	    } else if (currentSyntax->getDescription()=="Objective C") {
     		formatter->setObjCStyle();
         } else {
             formatter->setCStyle();
